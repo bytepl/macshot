a50cafa66e05aacc22546211c3fb2dec37746fd7
sw33tLie <sw33tlie@protonmail.com>
2026-03-11 14:38:58 +0000

    Initial Commit

---
+++ macshot/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macshot
+//
+//  Created by Paolo Arnolfo on 11/03/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
